5c510b966749516a847fb9d7b2f572dc51231661
VincentDS98 <disanto.vincenzo98@gmail.com>
2024-01-25 18:55:16 +0100

    template

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script >
+
+export default {
+  data() {
+    return { 
+      
+    };
+  },
+  
+}
+</script>
+
+<template>
+   <footer>
+      footer
+   </footer>
+    
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script >
+
+export default {
+  data() {
+    return { 
+     
+    };
+  },
+  
+}
+</script>
+
+<template>
+   <header>
+        header
+   </header>
+    
+</template>
+
+<style scoped>
+
+</style>
